426e1ad16676471b28cd57d58d14e2a56db4d0ac
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-04 18:44:25 -0600

    WIP: created a github user profile

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class DashboardController < ApplicationController
    def show
-     response = Faraday.get("https://api.github.com/users/#{current_user.username}/repos?access_token=#{current_user.oauth_token}")
 
-     @repos = JSON.parse(response.body)
+     response = Faraday.get("https://api.github.com/users/#{current_user.username}/starred?access_token=#{current_user.oauth_token}")
+    @starred_repos = JSON.parse(response.body)
+
+    response_2 = Faraday.get("https://api.github.com/users/#{current_user.username}/followers?access_token=#{current_user.oauth_token}")
+    @followers = JSON.parse(response_2.body)
+
+     response_3 = Faraday.get("https://api.github.com/users/#{current_user.username}/following?access_token=#{current_user.oauth_token}")
+
+     @following = JSON.parse(response_3.body)
   end
 end
